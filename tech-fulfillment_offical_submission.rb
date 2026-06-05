f3be94358443a5a333649ffe469b5463f0fe0ca9
Kevin Voshell <kevin.voshell@homechef.com>
2026-06-05 18:00:23 -0500

    final final

--- tech-fulfillment_offical_submission.rb
+++ tech-fulfillment_offical_submission.rb
@@ -1,19 +1,20 @@
 # Copy this into a rails console and you should see all test cases printed out
 
-def f(l, d, a, o, r, c)
+def f(l, a, o, r, c)
+  d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
   l.times { | i |
     return o if !o.dig(r, c) || c < 0 || r < 0
     o[r][c] = 1
     r, c = [r, c].zip(d[a]).map(&:sum) if i < l - 1
   }
-  o = f(l + 1, d, (l - 1) % 4, o, r, c)
+  o = f(l + 1, (l - 1) % 4, o, r, c)
 end
 
 def test
   inputs = [[3, 4], [4, 3], [4, 5], [5, 4], [7, 6], [5, 15], [15, 15], [15, 5], [3, 3], [1, 1]]
 
   inputs.each do |c,r|
-    result = f(2, [[1, 0], [0, 1], [-1, 0], [0, -1]], 0, Array.new(r) {[0]*c}, (r-1) / 2, (c-1) / 2)
+    result = f(2, 0, Array.new(r) {[0]*c}, (r-1) / 2, (c-1) / 2)
     result.each do |row|
       print row.join " "
       print "\n"
